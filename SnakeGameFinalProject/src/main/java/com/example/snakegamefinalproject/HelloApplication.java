e95f756886da80b7fec6354103775e384f8ca811
JustRodri15 <rodrje63@farmingdale.edu>
2024-05-02 03:10:56 -0400

    Final Project
    There are still minor problems with the coding that I still haven't figured out but overall it works

--- SnakeGameFinalProject/src/main/java/com/example/snakegamefinalproject/HelloApplication.java
+++ SnakeGameFinalProject/src/main/java/com/example/snakegamefinalproject/HelloApplication.java
@@ -2,6 +2,7 @@ package com.example.snakegamefinalproject;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -12,7 +13,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        stage.setTitle("Snake Game!");
         stage.setScene(scene);
         stage.show();
     }
